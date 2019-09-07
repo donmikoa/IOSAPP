521a61ed7d380223bebae46ee35f90ae48b327ed
michaeliyaomolere <mikoa04@gmail.com>
2019-09-07 07:13:25 -0400

    Initial Commit

---
+++ MikeIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MikeIOS
+//
+//  Created by Michael Iyaomolere on 9/7/19.
+//  Copyright © 2019 Michael Iyaomolere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
